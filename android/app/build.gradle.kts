3a03a658d612e1698a83fd17cd2f56a42a3cfd3b
Sisy Agnia <sisy.agniar10@gmail.com>
2025-04-09 07:13:19 +0700

    fresh start 9 april

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,13 +24,8 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-<<<<<<< HEAD
-        applicationId = "com.example.food_delivery_app"
-        ndkVersion = "28.0.13004108"
-=======
         applicationId = "com.madebysisy.recipeapp"
         // ndkVersion = "28.0.13004108"
->>>>>>> 0d91c6bc32ecbce2990e0aebb74691549ec1eac4
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
